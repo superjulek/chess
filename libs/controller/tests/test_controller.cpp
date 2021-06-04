fd69e03d13b6b42a16800f1fe419bb8140d70f84
jneuman <01132997@pw.edu.pl>
2021-06-04 19:03:31 +0200

    Actions and state queries for controller

--- libs/controller/tests/test_controller.cpp
+++ libs/controller/tests/test_controller.cpp
@@ -6,13 +6,43 @@
 
 #include <iostream>
 
-// Loader
+// Controller
 
-TEST(ControllerTests, DummyTest) {
+TEST(ControllerTests, LoadedTest) {
+  Controller my_controller = Controller(std::make_unique<ConsoleViewer>());
+
+  ASSERT_FALSE(my_controller.is_game_loaded());
+  my_controller.start_game(
+      std::make_unique<AIPlayer>(Piece::PieceColor::White),
+      std::make_unique<AIPlayer>(Piece::PieceColor::Black));
+  ASSERT_TRUE(my_controller.is_game_loaded());
+}
+
+TEST(ControllerTests, StateTest) {
   Controller my_controller = Controller(std::make_unique<ConsoleViewer>());
 
   my_controller.start_game(
       std::make_unique<AIPlayer>(Piece::PieceColor::White),
       std::make_unique<AIPlayer>(Piece::PieceColor::Black));
-  my_controller.display_board();
+  ASSERT_EQ(my_controller.get_game_state(), Game::GameState::Normal);
+}
+
+TEST(ControllerTests, PlayerTest) {
+  Controller my_controller = Controller(std::make_unique<ConsoleViewer>());
+
+  my_controller.start_game(
+      std::make_unique<AIPlayer>(Piece::PieceColor::White),
+      std::make_unique<AIPlayer>(Piece::PieceColor::Black));
+  ASSERT_EQ(my_controller.get_current_player(), Piece::PieceColor::White);
+}
+
+TEST(ControllerTests, PreviewTest) {
+  Controller my_controller = Controller(std::make_unique<ConsoleViewer>());
+
+  my_controller.start_game(
+      std::make_unique<AIPlayer>(Piece::PieceColor::White),
+      std::make_unique<AIPlayer>(Piece::PieceColor::Black));
+  ASSERT_FALSE(my_controller.is_preview());
+  my_controller.enter_preview();
+  ASSERT_TRUE(my_controller.is_preview());
 }
